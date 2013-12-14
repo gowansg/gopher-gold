78649d5c9962217dad0a379ea2cacdcd507f0857
Stephen Gowan <gowansg@gmail.com>
2013-12-14 16:01:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GopherGold::Application.config.secret_key_base = 'e9c481ee28be341453e68a454a3b4727c9de83a8b9f6ea2621cd23babe4f3a62e921dde4af02bc5d96c4be59872d817971e637d4a3f121b1849ca3624abedc64'
